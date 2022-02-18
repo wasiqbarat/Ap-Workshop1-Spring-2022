00735425f9f19f15493bef6d559632d932277e9b
Wasiq Barat <wasiqlilium1399@gmail.com>
2022-02-18 18:32:39 +0330

    Initial commit

--- src/calculator.java
+++ src/calculator.java
@@ -5,12 +5,20 @@ public class calculator {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Welcome to Calculator!");
+        System.out.println("Enter first number:");
+        int firstNum = scanner.nextInt();
+        System.out.println("Enter second num:");
+        int secondNum = scanner.nextInt();
+        if (secondNum == 0){
+            System.out.println("enter non-zero number!!!!");
+            return;
+        }
         System.out.println("Which operation do you want to perform: (    +  -  *  /    )");
         char operator = scanner.next().charAt(0);
         if (operator == '+' || operator == '-' || operator == '*' || operator == '/'){
             System.out.println("your choice is:" + operator);
         }else {
-            System.out.println("please enter correct operator!");
+            System.out.println("please enter valid operator!");
             return;
         }
     }
